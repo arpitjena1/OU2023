cd4c88f29ef4ee4675e6ee33075d4b8e66092e4e
Arpit Jena <99858283+arpitjena1@users.noreply.github.com>
2023-11-26 10:43:46 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,44 +13,18 @@
 #include "Programs/TestFunction/ForwardTest.h"
 #include "pros/vision.hpp"
 
-#define IS_FIFTEEN // uncomment for 15, comment for 18
+
 #define GTU_TURN_PRECISE DoubleBoundedPID({1.25, 0.005, 0.13, 0.17, 1}, getRadians(0.5), 3)
 bool isSkills = false;
-//#define TEST_TUNE_PID // uncomment to adjust pid using TuningDriver class. Should comment out RUN_AUTON
+Robot robot = getRobot15(isSkills);
 
-#ifdef IS_FIFTEEN
-    #define IS_THREE_TILE
-    Robot robot = getRobot15(isSkills);
-#else
-    #define IS_TWO_TILE
-    Robot robot = getRobot18(isSkills);
-#endif
-
-#ifdef IS_FIFTEEN
 FlywheelDriver driver(robot, TANK_DRIVE, 2500);
-#else
-CataDriver driver(robot, TANK_DRIVE);
-#endif
 
-//#define RUN_TEST
-//#define RUN_AUTON // uncomment to run auton, comment to run teleop / actual comp
-//#define TUNE_FLYWHEEL // uncomment to run flywheel tuning program intsead, comment to disable this
+
 
 using namespace pros;
 
-bool centerButtonReady = false;
 
-void ready() {
-    centerButtonReady = true;
-}
-
-void lowerCata() {
-
-    // no cata to lower
-    if (!robot.cata) return;
-
-    shootCataNonblocking(robot);
-}
 pros::Vision vis(20);
 void VisionAim() {
   // Sets zero point to center of FOV
@@ -96,24 +70,10 @@ void VisionAim() {
 void initialize() {
 
     pros::lcd::initialize();
-    pros::lcd::register_btn1_cb (ready);
-    pros::lcd::register_btn0_cb(lowerCata);
+    robot.localizer->init();
 
     
-    if (robot.shooterFlap) robot.shooterFlap->set_value(true); // start flap up
-
-	robot.localizer->init();
-
-    #ifdef RUN_AUTON
-    #ifndef TUNE_FLYWHEEL
-    while (!centerButtonReady) {
-
-        pros::lcd::print(3, "Heading (deg): %f", robot.localizer->getHeading() * 180 / 3.1415);
-        
-        pros::delay(10);
-    }
-    #endif
-    #endif
+	
 
 }
 
@@ -152,31 +112,11 @@ void autonomous() {
 
 void opcontrol() {
 
-    #ifdef TUNE_FLYWHEEL
-    tuneFlywheel(robot, driver.controller);
-    return;
-    #endif
-
-
-
-	#ifdef RUN_AUTON
-	autonomous();
-	return;
-	#endif
-    pros::Motor motor1(1);
-    pros::Motor motor2(2);
-    pros::Motor motor3(3);
-    pros::ADIDigitalOut puncher1('A');
-    pros::ADIDigitalOut puncher2('B');
     while(true){
-        //puncher1.set_value(true) && puncher2.set_value(true);
-        //puncher1.set_value(false) && puncher2.set_value(false);
-       motor1.move_voltage(-12000);
-       motor2.move_voltage(12000);
-       motor3.move_voltage(3000);
+        
     }
     
-
-	//driver.runDriver();
+   //run driver control
+   driver.runDriver();
 	
 }
